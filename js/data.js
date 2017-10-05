3cb81d4d1e61a5f0630d9acd360d1ba012c577e5
Bryan Wong <bryanwong@bryans-mbp.home>
2017-10-04 23:31:26 -0700

    add shit for mobile

--- js/data.js
+++ js/data.js
@@ -7,17 +7,22 @@ function getTexture(path){
 	return texture;
 }
 
+var scaleDown = 1;
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+	scaleDown = .7;
+}
+
 var ImageData = {
 	'sardines' : {
 		'texture' : getTexture('assets/images/sardines.png'),
 		'aspect' : 758/1300,
-		'scale' : 9
+		'scale' : 9*scaleDown
 	},
 
 	'robot' : {
 		'texture' : getTexture('assets/images/maciej.png'),
 		'aspect' : 581/385,
-		'scale' : 5
+		'scale' : 5*scaleDown
 	}
 }
 
